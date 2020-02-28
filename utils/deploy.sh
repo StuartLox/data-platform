91c208cbdaacf8616803eba01bb22606862b2a48
StuartLox <stuart.loxton@xinja.com.au>
2020-02-28 18:18:42 +1100

    Update deploy scripts

--- utils/deploy.sh
+++ utils/deploy.sh
@@ -10,7 +10,7 @@ TF_VAR_environment=dev \
 TF_VAR_account=prod \
 TF_VAR_kube_config=$HOME/.kube/config \
 TF_VAR_region=ap-southeast-2 \
-terragrunt $ACTION
+terragrunt $ACTION -lock=false
 
 #==========#
 # CLEAN UP #
--- utils/kafka_operator_init.sh
+++ utils/kafka_operator_init.sh
@@ -7,4 +7,7 @@ kubectl create serviceaccount tiller -n kube-system
 kubectl create clusterrolebinding tiller --clusterrole=cluster-admin --serviceaccount kube-system:tiller
 helm init --service-account tiller
 
+helm install -f ./providers/aws.yaml --name operator --namespace operator --set operator.enabled=true ./confluent-operator
 helm install -f ./providers/aws.yaml --name zookeeper --namespace operator --set zookeeper.enabled=true ./confluent-operator
+helm install -f ./providers/aws.yaml --name kafka --namespace operator --set kafka.enabled=true ./confluent-operator
+helm install -f ./providers/aws.yaml --name schemaregistry --namespace operator --set schemaregistry.enabled=true ./confluent-operator
